b276cfd4232f32e5a9ee68a7f5b5968635230db8
Lucas Frota Barroso <122243103+lucasfrotabarroso@users.noreply.github.com>
2023-04-14 19:28:29 -0300

    Update Q3_LucasBarroso.java

--- av2_LucasBarroso/Q3_LucasBarroso.java
+++ av2_LucasBarroso/Q3_LucasBarroso.java
@@ -1,5 +1,23 @@
+
 package av2_LucasBarroso;
+import java.util.*;
 
 public class Q3_LucasBarroso {
-
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Digite a string:");
+        String a = s.nextLine();
+        String [] tokens = a.split(" "); //vai separar a minha frase em uma substring sempre que estiver um espaco 
+        for (String token : tokens) { //o token e cada palara e o toKENS e a frase toda
+       
+            if (token.startsWith("s") || token.startsWith("S")) { 
+            	System.out.println("os tokens que comecam com s : ");
+            
+            	System.out.println(token);
+            }
+            
+            	                   
+        }
+    }
 }
+
